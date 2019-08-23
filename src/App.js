21f4d29565130ff47d8f253916f376ab1143c1cd
Debojyoti Chatterjee <debojyoti@glionconsulting.com>
2019-08-23 20:02:33 +0530

    updated with the admin panel and enabled the edit guesser

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Admin, Resource, EditGuesser } from "react-admin";
+import { UserList } from './users';
+import jsonServerProvider from "ra-data-json-server";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const dataProvider =
+  jsonServerProvider("https://jsonplaceholder.typicode.com");
+
+class App extends Component {
+  render() {
+    return (
+      <Admin dataProvider={dataProvider}>
+        <Resource name="users" list={UserList} edit={EditGuesser}/>
+      </Admin>
+    );
+  }
 }
-
 export default App;
